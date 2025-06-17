d4812e1049dfc38267513231fe26ec5082f964d1
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-17 18:00:21 +0000

    testing

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("sub2ades") 
+file = open("index.html", "w")
+
+file.write("<html>")
+file.write("<head>")
+file.write("<title>My Webpage</title>")
+file.write("</head>")
+file.write("<body>")
+file.write("<h1>Welcome to my webpage!</h1>")
+file.write("</body>")
+file.write("</html>")
+
+file.close()
