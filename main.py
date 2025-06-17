94a962de9413673d5b33a484aa18a73005a3d660
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-17 23:02:40 +0000

    more testing lol

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from pyscript import document
-print("sub2ades")
 output_div = document.querySelector("#textarea")
-output_div.innerText = "sub2ades"
+
+def _print(text):
+    output_div.innerText = text
+
+#############
+
+import random
+
+randomnumber = random.randint(1,2)
+
+if randomnumber == 1:
+    _print("sub2ades")
+else: 
+    _print("unsub2ades")
