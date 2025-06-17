48b52d76c8ec7842552b5279f00efeb7c4875fb8
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-17 23:07:56 +0000

    testing

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from pyscript import document
 output_div = document.querySelector("#textarea")
 
-def _print(text):
+def _print(text): # USE THIS INSTEAD OF print() IF YOU WANT IT TO SHOW
     output_div.innerText = text
 
-#############
+#### ^^ HTML SETUP DO NOT TOUCH ^^ ####
 
 import random
+randomnumber = random.randint(1,8)
 
-randomnumber = random.randint(1,2)
-
-if randomnumber == 1:
-    _print("sub2ades")
-else: 
-    _print("unsub2ades")
+_print("wow")
+_print("wo2w")
