f1365b6a34d7311cab659fe6deba2f93f62ccc62
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-17 18:02:00 +0000

    woah

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+# Open the file in write mode
 file = open("index.html", "w")
 
+# Write HTML content
 file.write("<html>")
 file.write("<head>")
 file.write("<title>My Webpage</title>")
@@ -9,4 +11,7 @@ file.write("<h1>Welcome to my webpage!</h1>")
 file.write("</body>")
 file.write("</html>")
 
+# Close the file
 file.close()
+
+print("HTML file successfully written.")
