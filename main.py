0d3ccbd942d5a941de46d7d915471e16f57c8792
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-18 01:13:31 +0000

    fixed!!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyscript import document
-output_div = document.querySelector("#textarea")
+output_div = document.querySelector(".textarea")
 
 printList = []
 
@@ -10,12 +10,17 @@ def _print(text): # USE THIS INSTEAD OF print() IF YOU WANT IT TO SHOW
 
 import random
 
-print("debug4")
-
 _print("DEAD WEST")
 _print("Welcome to the Game")
 _print("be sure to sub 2 ades!!!")
 _print("if you don't i will steal ur kneecaps")
+_print("How to Play" \
+"The Goal of the Game: Reach 100km" \
+"Raid Houses or Cities to get items to sell or keep in your Sack which contains 25kg worth of items" \
+"You start off with a Crowbar that can be used to pry open House Doors and Kill Zombies" \
+"Add Coal to the Furnace in the Train to add 'Ticks' of fuel" \
+"Good Luck")
+_print("Type [y] to see Complex Instructions otherwise type [n] to Start")
 
 #### vv HTML SETUP DO NOT TOUCH vv ####
 
