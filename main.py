c50f34f22ea58079c31501162bbe8c67f9d596c3
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-18 00:33:33 +0000

    debugging more

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ _print("DEAD WEST")
 _print("Welcome to the Game")
 _print("be sure to sub 2 ades!!!")
 _print("if you don't i will steal ur kneecaps")
+
 _print("The year is 1884, the World has been taken over by an Zombie Apocolypse, the only safe spot is in Mexico exactly 100km away from you in the USA. You plan to travel there to be safe but the only way is through a train. You go to find a driver only to realize that they all turned into Zombies. You are on your own and have to find everything you need such as fuel. Good Luck")
 
 _print("Type 'y' to see Complex Instructions otherwise type n to Start")
