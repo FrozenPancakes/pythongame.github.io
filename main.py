c525332b7fcf9c66ad2ac5961f14ef0ad557c4f7
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-18 01:22:06 +0000

    texts

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ _print("DEAD WEST")
 _print("Welcome to the Game")
 _print("be sure to sub 2 ades!!!")
 _print("if you don't i will steal ur kneecaps")
-_print("How to Play" \
-"The Goal of the Game: Reach 100km" \
-"Raid Houses or Cities to get items to sell or keep in your Sack which contains 25kg worth of items" \
-"You start off with a Crowbar that can be used to pry open House Doors and Kill Zombies" \
-"Add Coal to the Furnace in the Train to add 'Ticks' of fuel" \
-"Good Luck")
+_print(" ")
+
+_print("The Goal of the Game: Reach 100km")
+_print("Raid Houses or Cities to get items to sell or keep in your Sack which contains 25kg worth of items")
+_print("You start off with a Crowbar that can be used to pry open House Doors and Kill Zombies")
+_print("Add Coal to the Furnace in the Train to add 'Ticks' of fue")
+_print("Good Luck!")
+
+_print(" ")
 _print("Type [y] to see Complex Instructions otherwise type [n] to Start")
 
 #### vv HTML SETUP DO NOT TOUCH vv ####
