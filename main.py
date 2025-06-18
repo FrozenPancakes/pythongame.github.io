b9f2de7a0c9428375a0003b698e5e763cd32f273
FrozenPancakes <156951034+FrozenPancakes@users.noreply.github.com>
2025-06-18 00:44:43 +0000

    aa

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def _print(text): # USE THIS INSTEAD OF print() IF YOU WANT IT TO SHOW
 
 import random
 
-print("debug2")
+print("debug3")
 
 _print("DEAD WEST")
 _print("Welcome to the Game")
@@ -19,7 +19,7 @@ _print("if you don't i will steal ur kneecaps")
 
 _print("The year is 1884, the World has been taken over by an Zombie Apocolypse, the only safe spot is in Mexico exactly 100km away from you in the USA. You plan to travel there to be safe but the only way is through a train. You go to find a driver only to realize that they all turned into Zombies. You are on your own and have to find everything you need such as fuel. Good Luck")
 
-_print("Type 'y' to see Complex Instructions otherwise type n to Start")
+_print("Type [y] to see Complex Instructions otherwise type [n] to Start")
 
 #### vv HTML SETUP DO NOT TOUCH vv ####
 
